533895703c064ecb72993447d656cb6cb9e13200
midoliy <midoliy@outlook.jp>
2022-03-05 21:42:27 +0900

    update

---
+++ src/Owl.Edge.WebDriver/Trunk.fs
@@ -0,0 +1,9 @@
+﻿namespace Owl.Edge.WebDriver
+
+module Trunk =
+  let get_x64_driver_url version = sprintf $"https://msedgedriver.azureedge.net/%s{version}/edgedriver_win64.zip"
+  let get_x86_driver_url version = sprintf $"https://msedgedriver.azureedge.net/%s{version}/edgedriver_win32.zip"
+  let get_linux_driver_url version = sprintf $"https://msedgedriver.azureedge.net/%s{version}/edgedriver_linux64.zip"
+  let get_mac_driver_url version = sprintf $"https://msedgedriver.azureedge.net/%s{version}/edgedriver_mac64.zip"
+  let get_arm_driver_url version = sprintf $"https://msedgedriver.azureedge.net/%s{version}/edgedriver_arm64.zip"
+
